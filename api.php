6d896d6cde4a01d9289ca69a3f158749c73de0bb
Özgürcan <102454358+EironeMarian@users.noreply.github.com>
2023-02-25 12:50:28 +0300

    Add files via upload

--- api.php
+++ api.php
@@ -11,11 +11,6 @@
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-
-<h1>Spotify <br>Müzik Kapağı İndirici</h1>
 
 </body>
 </html>
@@ -25,7 +20,13 @@ $trackurl = $_POST["url"];
 $xurl = "https://open.spotify.com/oembed?url=".$trackurl;
 $kaynak = file_get_contents($xurl);
 $data = json_decode($kaynak);
+$tracknam = $data->title;
 $cikolata = $data->thumbnail_url;
+echo "<h1>$tracknam</h1>"; 
 echo "<img src='$cikolata' >"; 
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<a href='$cikolata' class='download-btn' download>Resim Dosyasını İndir</a>";
 
 ?>
